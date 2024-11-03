eb7be295891937ea3177a3b10fe10233d7579d4d
Alana-Murry07 <89994505+alana-murry@users.noreply.github.com>
2024-11-02 18:55:16 -0700

    Initial Commit

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }   
+
+        [Required]
+        [StringLength(100)]
+        public string Author { get; set; }
+
+        public bool IsRead { get; set; }
+
+    }
+}
